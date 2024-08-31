66d6b043d030b3f828d2c515e7553d39bd12c38a
Jeremy Fuentes <jeremyfuentes201@gmail.com>
2024-08-30 18:06:58 -0600

    MOdificacion de carpetas y agregado de tabla productos

--- CapaDatos/ProductosDAL.cs
+++ CapaDatos/ProductosDAL.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CapaLogica
+namespace CapaDatos
 {
-    public class Class1
+    internal class ProductosDAL
     {
+
     }
 }
--- CapaLogica/ProductoRepository.cs
+++ CapaLogica/ProductoRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CapaDatos
+namespace CapaLogica
 {
-    public class Class1
+    public class ProductoRepository
     {
     }
 }
